06544db1cf9f4ba6c5bac531746fa6a03bcbace8
SilentCyborgs <silentcyborgs@gmail.com>
2014-12-10 02:53:02 +0000

    *added basic random spawn script between x = -5, 5 and z = -5, 5 *removed character controller prefabs, now using capsule *need motor controller and character controller

---
+++ SilentCyborgProject/Assets/Scripts/RandomSpawnScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSpawnScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3 (Random.Range (-5.0f, 5.0f), 1.05f, Random.Range (-5.0f, 5.0f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
