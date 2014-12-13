9ab6a4f540e6e4e0b6b39135744086adbbdbd950
SilentCyborgs <silentcyborgs@gmail.com>
2014-12-13 07:23:13 +0000

    *Updated Random Spawn script, not currently working *Capsule Stored As Prefab Called Player, This To Be Replaced But Keep Name As Player For Above Mentioned Script

--- SilentCyborgProject/Assets/Scripts/RandomSpawnScript.cs
+++ SilentCyborgProject/Assets/Scripts/RandomSpawnScript.cs
@@ -2,11 +2,21 @@
 using System.Collections;
 
 public class RandomSpawnScript : MonoBehaviour {
+	public float XRandomMin = -5.0f;
+	public float XRandomMax = 5.0f;
+	public float ZRandomMin = -5.0f;
+	public float ZRandomMax = 5.0f;
 
 	// Use this for initialization
 	void Start () {
-		transform.position = new Vector3 (Random.Range (-5.0f, 5.0f), 1.05f, Random.Range (-5.0f, 5.0f));
-	}
+		float XPosition = Random.Range (XRandomMin, XRandomMax);
+		float ZPosition = Random.Range (ZRandomMin, ZRandomMax);
+		Vector3 PlayerPosition = new Vector3(XPosition, 0f, ZPosition);
+		PlayerPosition.y = Terrain.activeTerrain.SampleHeight(PlayerPosition) + Terrain.activeTerrain.GetPosition().y;
+		PlayerPosition.y += 0.5f;
+
+		GameObject Player = Instantiate(Resources.Load("Prefabs/Player"),PlayerPosition,Quaternion.identity) as GameObject;
+		}
 	
 	// Update is called once per frame
 	void Update () {
