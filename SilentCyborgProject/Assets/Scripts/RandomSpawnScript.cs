269678b64ca6c4006dee64ca3bc218a2970d75bb
SilentCyborgs <silentcyborgs@gmail.com>
2014-12-13 04:03:39 +0000

    Update RandomSpawnScript.cs

--- SilentCyborgProject/Assets/Scripts/RandomSpawnScript.cs
+++ SilentCyborgProject/Assets/Scripts/RandomSpawnScript.cs
@@ -5,7 +5,11 @@ public class RandomSpawnScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		transform.position = new Vector3 (Random.Range (-5.0f, 5.0f), 1.05f, Random.Range (-5.0f, 5.0f));
+Public var XRangeMin = -5.0f
+Public var XRangeMax = 5.0f
+Public var ZRangeMin = -5.0f
+Public var ZRangeMax = 5.0f
+		transform.position = new Vector3 (Random.Range (XRangeMin, XRangeMax), 1.05f, Random.Range (ZRangeMin, ZRangeMax));
 	}
 	
 	// Update is called once per frame
