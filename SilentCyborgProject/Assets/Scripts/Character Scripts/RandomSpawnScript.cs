f58ff994ffa09e6e7bc9bf57d2febc1c632e23aa
SilentCyborgs <silentcyborgs@gmail.com>
2014-12-21 08:28:07 +0000

    *Fixed and working player spawn script, will need to be modified later to spawn into random spawn array locations

--- SilentCyborgProject/Assets/Scripts/Character Scripts/RandomSpawnScript.cs
+++ SilentCyborgProject/Assets/Scripts/Character Scripts/RandomSpawnScript.cs
@@ -6,20 +6,19 @@ public class RandomSpawnScript : MonoBehaviour {
 	public float XRandomMax = 5.0f;
 	public float ZRandomMin = -5.0f;
 	public float ZRandomMax = 5.0f;
-
+	private bool PlayerSpawned = false; 
 	// Use this for initialization
 	void Start () {
-		float XPosition = Random.Range (XRandomMin, XRandomMax);
-		float ZPosition = Random.Range (ZRandomMin, ZRandomMax);
-		Vector3 PlayerPosition = new Vector3(XPosition, 0f, ZPosition);
-		PlayerPosition.y = Terrain.activeTerrain.SampleHeight(PlayerPosition) + Terrain.activeTerrain.GetPosition().y;
-		PlayerPosition.y += 0.5f;
-
-		GameObject Player = Instantiate(Resources.Load("Prefabs/Player"),PlayerPosition,Quaternion.identity) as GameObject;
-		}
-
-	// Update is called once per frame
-	void Update () {
-	
+		if (PlayerSpawned != true) {
+						float XPosition = Random.Range (XRandomMin, XRandomMax);
+						float ZPosition = Random.Range (ZRandomMin, ZRandomMax);
+						Vector3 PlayerPosition = new Vector3 (XPosition, 0f, ZPosition);
+						PlayerPosition.y = Terrain.activeTerrain.SampleHeight (PlayerPosition) + Terrain.activeTerrain.GetPosition ().y;
+						PlayerPosition.y += 1.0f;
+						GameObject Player = Instantiate (Resources.Load ("Prefabs/Player"), PlayerPosition, Quaternion.identity) as GameObject;
+						PlayerSpawned = true;
+				}
 	}
+	// Update is called once per frame
+	void Update () {}
 }
