34663451dd2d60334245a16a8a0daccf1f99147c
SilentCyborgs <silentcyborgs@gmail.com>
2015-01-10 21:21:14 +0000

    *Water plane added and textured - currently able to walk through the plane so needs to be scripted to add swimming need to change colour dependant on lighting as it currently glows at night and also need to replace it eventually with better model. *Sun flare added with rotation script for night and day, needs more complexity for time of day and seasons. *RAIN asset added for AI in the future if we choose to use a complete solution. *Other materials and assets added for the features above.

---
+++ SilentCyborgProject/Assets/Scripts/Nature Scripts/DayNightCycle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNightCycle : MonoBehaviour {
+	public float Speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Speed*Time.deltaTime,0,0);
+	}
+}
