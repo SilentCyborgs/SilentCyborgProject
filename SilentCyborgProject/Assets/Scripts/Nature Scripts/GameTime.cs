b3009b9461badebb822123782164d89328f98f5d
SilentCyborgs <silentcyborgs@gmail.com>
2015-01-12 08:57:03 +0000

    *Started Real-Time Script

--- SilentCyborgProject/Assets/Scripts/Nature Scripts/GameTime.cs
+++ SilentCyborgProject/Assets/Scripts/Nature Scripts/GameTime.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EmptyScript2 : MonoBehaviour {
+public class GameTime : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ SilentCyborgProject/Assets/Scripts/Nature Scripts/Sun.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu ("Environment/Sun")]
+
+public class Sun : MonoBehaviour {
+
+	public float MaxLightBrightness;
+	public float MinLightBrightness;
+
+	public float MaxFlareBrightness;
+	public float MinFlareBrightness;
+
+
+
+}
